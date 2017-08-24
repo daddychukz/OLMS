50fc2dc4e980c935aadd4d8f2f8a54b5e7487352
daddychuks <durugo_chuks@yahoo.com>
2017-08-24 07:54:07 +0100

    move all routes to the routes folder

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,9 @@
+// bringing in dependencies
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-const http = require('http');
+// const http = require('http');
+const routes = require('./routes');
 
 const app = express();
 
@@ -9,23 +11,18 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// connect all routes to application
+app.use('/', routes);
+
 const port = +process.env.PORT || 3000;
 app.set('port', port);
 
-http.createServer(app).listen(port, () => {
-  console.log(`The server is listening on port ${port}`);
+// http.createServer(app).listen(port, () => {
+//   console.log(`The server is listening on port ${port}`);
+// });
+
+// Turn on the server
+app.listen(port, () => {
+  console.log(`The App is listening on port ${port}`);
 });
 
-/* GET home page. */
-app.get('/', (req, res) =>
-  res.status(200).send({
-    message: 'Online Library Management System',
-  })
-);
-
-// A catch-all routes not define.
-app.get('*', (req, res) => res.status(404).send(
-  'Invalid Route...'
-));
-
-module.exports = app;
