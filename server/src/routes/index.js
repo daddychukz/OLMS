c3c498ba64c8e220c34aa156dad70919a179b0ca
daddychuks <durugo_chuks@yahoo.com>
2017-08-24 07:55:01 +0100

    create routes directories

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,4 +1,18 @@
-const express = require('express');
+const router = require('express').Router();
 
-const router = express.Router();
+/* GET home page. */
+router.get('/', (req, res) =>
+  res.status(200).send({
+    message: 'Online Library Management System',
+  })
+);
 
+/* All API Routes */
+
+
+// A catch-all routes not define.
+router.get('*', (req, res) => res.status(404).send(
+  'Invalid Route...'
+));
+
+module.exports = router;
