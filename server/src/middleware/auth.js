1c672c4c64ba1fd437558e5047cd1e06d3dde176
daddychuks <durugo_chuks@yahoo.com>
2017-08-27 06:00:13 +0100

    update authentication status code and error message

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -4,21 +4,39 @@ require('dotenv').config();
 const secret = process.env.SECRET;
 
 const Auth = {
+  // function to authenticate access to users with a token
   verify(req, res, next) {
-    if (req.headers.authorization) {
-      next();
+    const token = req.headers.authorization;
+    if (token) {
+      jwt.verify(token, secret, (err, decoded) => {
+        if (err) {
+          res.status(401).send({ message: 'You do not have Permission to this Page' });
+        } else {
+          req.decoded = decoded;
+          next();
+        }
+      });
     } else {
-      res.status(401).send({ message: 'You are currently unauthorized' });
+      res.status(401).send({ message: 'No token provided' });
     }
   },
+  // function to authenticate access to Admin only with a token
   isAdmin(req, res, next) {
-    const decoded = jwt.verify(req.headers.authorization, secret);
-    if (decoded.isAdmin === true) {
-      next();
+    const token = req.headers.authorization;
+    if (token) {
+      jwt.verify(token, secret, (err, decoded) => {
+        if (err) {
+          res.status(401).send({ message: 'Invalid Token' });
+        } else if (decoded.isAdmin === true) {
+          next();
+        } else {
+          res.status(401).send({ message: 'You do not have Permission to this Page' });
+        }
+      });
     } else {
-      res.status(401).send({ message: 'Access Denied! You are currently unauthorized' });
+      res.status(401).send({ message: 'No token provided' });
     }
-  },
+  }
 };
 
 module.exports = {
