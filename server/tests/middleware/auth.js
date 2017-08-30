a094bb4436f4ef80ee9a5d79af6fe52440cc6d32
daddychuks <durugo_chuks@yahoo.com>
2017-08-30 07:33:59 +0100

    add test for middleware

---
+++ server/tests/middleware/auth.js
@@ -0,0 +1,80 @@
+// /**
+//  * API Endpoint Tests
+//  */
+
+const request = require('supertest');
+const app = require('../../src/app');
+const models = require('../../src/models/');
+const expect = require('chai').expect;
+const fakeData = require('../helpers/fakeData');
+
+let authToken;
+
+describe('Middleware Authentication Tests', () => {
+  before((done) => {
+    models.user.create(fakeData.userOne)
+      .then(() => {
+        models.book.create(fakeData.book)
+          .then(() => {
+            request(app)
+              .post('/api/users/signin')
+              .send({
+                email: fakeData.userOne.email,
+                password: fakeData.userOne.password
+              })
+              .end((err, res) => {
+                authToken = res.body.token;
+                done();
+              });
+          });
+      });
+  });
+
+  after(() =>
+    models
+      .book
+      .destroy({
+        cascade: true,
+        truncate: true
+      }),
+
+  models
+    .user
+    .destroy({
+      cascade: true,
+      truncate: true
+    })
+  );
+
+  it('grant access request for users to view books with valid token', (done) => {
+    request(app)
+      .get('/api/books')
+      .set('authorization', authToken)
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
+      });
+  });
+
+  it('should not grant access request for users to view books without token', (done) => {
+    request(app)
+      .get('/api/books')
+      .end((err, res) => {
+        expect(res.status).to.equal(401);
+        expect(res.body.message).to.equal('No token provided');
+        done();
+      });
+  });
+
+  it('deny access to books request for users with invalid token', (done) => {
+    const randomToken = 'kzsjvduvgdbvjask123bn';
+    request(app)
+      .get('/api/books')
+      .set('Authorization', randomToken)
+      .end((err, res) => {
+        expect(res.status).to.equal(401);
+        expect(res.body.message).to.equal('You do not have Permission to this Page');
+        done();
+      });
+  });
+});
